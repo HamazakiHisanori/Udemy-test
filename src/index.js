5d3d86aefd56c41f64e56f106bd22c29714c0509
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-08-23 18:09:29 +0900

    追加機能の実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //要素取得
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  li.className = "list";
+
+  //p作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //button生成
+
+  //list生成
+  li.appendChild(p);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
